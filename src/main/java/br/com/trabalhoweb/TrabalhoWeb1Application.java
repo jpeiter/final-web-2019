8c6104a1a54f05048d2087f63158ac361cc046a0
Jean Felipe Peiter <jpeiter13@gmail.com>
2019-05-07 19:37:01 -0300

    First Commit

---
+++ src/main/java/br/com/trabalhoweb/TrabalhoWeb1Application.java
@@ -0,0 +1,13 @@
+package br.com.trabalhoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoWeb1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrabalhoWeb1Application.class, args);
+    }
+
+}
